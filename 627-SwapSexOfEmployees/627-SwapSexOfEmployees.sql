ecdc3d476fe02dcc6ff1f022bca1daec0b949095
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-05-12 02:25:40 -0400

    Update 627-SwapSexOfEmployees.sql

--- 627-SwapSexOfEmployees/627-SwapSexOfEmployees.sql
+++ 627-SwapSexOfEmployees/627-SwapSexOfEmployees.sql
@@ -1,7 +1,12 @@
 -- Write your PostgreSQL query statement below
+/*
+1. new syntax. added to my notes
+2. NO BRACKETS ON CASE!
+3. need to keep in touch with sql or else i will forget.
 
--- new syntax. added to my notes
-
+query: a query is something that is sent to the database. a query can be one that modifies value, or one that requests and returns value.
+update itself does not return values from the database, it modifies values.
+*/
 UPDATE salary
 SET sex = 
 CASE 
